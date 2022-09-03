55d746fafdca1038b163afe0a2ae2623ddd6106c
Emerson Millard <emillard.code@gmail.com>
2022-09-03 00:02:31 -0700

    Initial commit

---
+++ src/main/java/com/project/springcloudstreamkafkapublisherjt/SpringCloudStreamKafkaPublisherJtApplication.java
@@ -0,0 +1,13 @@
+package com.project.springcloudstreamkafkapublisherjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamKafkaPublisherJtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamKafkaPublisherJtApplication.class, args);
+    }
+
+}
